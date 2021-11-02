26750bb991d0349609a79573bace3e00bcb3d034
Wesley455 <wesley@drterapia.com>
2021-11-01 22:38:25 -0300

    challenge componentizando a aplicacao finished

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,43 @@
-export function SideBar() {
-  // Complete aqui
-}
+import React from "react";
+
+import { Button } from "./Button";
+
+type GenreResponseProps = {
+  id: number;
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+  title: string;
+};
+
+type SideBarProps = {
+  genres: GenreResponseProps[];
+  handleClickButton: (id: number) => void;
+  selectedGenreId: number;
+};
+
+const SideBar: React.FC<SideBarProps> = ({
+  genres,
+  handleClickButton,
+  selectedGenreId,
+}) => {
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export { SideBar };
